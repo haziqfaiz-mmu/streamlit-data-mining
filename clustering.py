0de43b0bc3dc72052702148b6ed0572657d059ef
haziqfaiz-mmu <1201302740@student.mmu.edu.my>
2023-01-14 16:40:01 +0800

    delete confusion matrix

--- clustering.py
+++ clustering.py
@@ -115,7 +115,7 @@ def none_option(neighbors, X_clus, y_clus):
             st.write("The best score is ", knn_gscv.best_score_)
 
             st.write("Then we plot the confusion matrix, ROC Curve and precision recall curve uing the best number of neighbors")
-            st.subheader("Confusion Matrix")
+            #st.subheader("Confusion Matrix")
             #plot_confusion_matrix(knn_gscv.best_estimator_, x_test, y_test)
             predictions=knn_gscv.best_estimator_.fit(x_train, y_train).predict(x_test)
             cm = confusion_matrix(y_test, predictions, labels=knn_gscv.classes_)
