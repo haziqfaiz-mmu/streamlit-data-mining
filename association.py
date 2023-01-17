5ac5c13b933c9d34f959bc388413d077ef76f97a
haziqfaiz <1201302740@student.mmu.edu.my>
2023-01-18 03:42:19 +0800

    betulkan association

--- association.py
+++ association.py
@@ -1,6 +1,28 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def association(df):
-    if st.sidebar.button("Run model", key='run'):
-        print("ass")
+    st.header("Apriori Algorithm")
+    if st.sidebar.button("Show Results", key='run'):
+        
+        association_results = np.load('association_results.npy',allow_pickle=True)
+        cnt = 0
+        for item in association_results:
+            cnt += 1
+            # first index of the inner list
+            # Contains base item and add item
+            pair = item[0] 
+            items = [x for x in pair]
+            st.write("(Rule " , str(cnt) + ") " , items[0] , " -> " , items[1])
+
+            #second index of the inner list
+            st.write("Support: " , str(round(item[1],3)))
+
+            #third index of the list located at 0th
+            #of the third index of the inner list
+
+            st.write("Confidence: " , str(round(item[2][0][2],4)))
+            st.write("Lift: " , str(round(item[2][0][3],4)))
+            st.write("=====================================") 
