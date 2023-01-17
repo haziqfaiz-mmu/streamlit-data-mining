75078a9e430d3ac9ddad2231ab75bb42fac4317f
haziqfaiz <1201302740@student.mmu.edu.my>
2023-01-18 02:19:30 +0800

    cluetering

--- kmeans.py
+++ kmeans.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 
 def kmeans(df):
     st.markdown("Perform Clustering using K-Means CLustering")
